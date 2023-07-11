9d8ce9e6a3cb39dca702a8c89d359a33b872f9c8
xandao06 <alexandre@cprautomacao.com.br>
2023-07-10 22:19:18 -0300

    a

--- CPR/wwwroot/js/script.js
+++ CPR/wwwroot/js/script.js
@@ -3,14 +3,21 @@
 
 // Write your JavaScript code.
 
-function changeIcon() {
-    var icon = document.getElementById('icon');
-
+function changeIcon(button) {
+    var icon = button.querySelector('i');
     if (icon.classList.contains('bi-pencil-square')) {
         icon.classList.remove('bi-pencil-square');
-        icon.classList.add('bi-check-square');
+        icon.classList.add('bi-exclamation-square-fill');
     } else {
-        icon.classList.remove('bi-check-square');
+        icon.classList.remove('bi-exclamation-square-fill');
         icon.classList.add('bi-pencil-square');
     }
 }
+
+document.getElementById('open-button').addEventListener('click', function () {
+    document.getElementById('overlay').style.display = 'block';
+});
+
+document.getElementById('close-button').addEventListener('click', function () {
+    document.getElementById('overlay').style.display = 'none';
+});
