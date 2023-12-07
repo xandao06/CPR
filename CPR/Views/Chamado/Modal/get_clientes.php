a6b2f4546a9c7155ff3009dc51507c20c92e68ba
xandao06 <alexandre@cprautomacao.com.br>
2023-12-06 21:13:59 -0300

    Tentativa de fazer funcionar option picker nos clientes

---
+++ CPR/Views/Chamado/Modal/get_clientes.php
@@ -0,0 +1,27 @@
+﻿<?php
+$servername = "seu_servidor_mysql";
+$username = "seu_usuario_mysql";
+$password = "sua_senha_mysql";
+$dbname = "seu_banco_de_dados";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
+
+$sql = "SELECT nome FROM clientes";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $clientes = array();
+    while ($row = $result->fetch_assoc()) {
+        $clientes[] = $row["nome"];
+    }
+    echo json_encode($clientes);
+} else {
+    echo "0 resultados";
+}
+
+$conn->close();
+?>
